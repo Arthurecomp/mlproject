ec97043ea5aaff9536fbbd88e5ed259e02c4d672
arthur <arthurecomp22@gmail.com>
2026-03-02 22:34:32 -0300

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str= os.path.join('artifacts', 'train.csv')
@@ -17,7 +25,7 @@ class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
 
-    def iniate_data_ingestion(self):
+    def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
             df = pd.read_csv('notebook\data\stud.csv')
@@ -42,6 +50,13 @@ class DataIngestion:
             raise CustomException(e,sys)
 
 
-if __name__== "__main__":
-    obj = DataIngestion()
-    obj.iniate_data_ingestion()
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_array=train_arr, test_array=test_arr))
+
